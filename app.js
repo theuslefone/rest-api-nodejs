9e9fe21ac1e0e60d6236daa8e18c9302611b6528
Matheus Ramos <desenvolvimento2@bisa.com.br>
2022-05-24 15:03:43 -0300

    Faz tratamento de erro

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const rotaProduto = require('./routes/produtos');
 const rotaPedido = require('./routes/pedidos');
 
+app.use(morgan('dev'));
+
 app.use('/produtos', rotaProduto);
 app.use('/pedidos', rotaPedido);
+
+app.use((req, res, next) => {
+    const erro = new Error('Não encontrado!')
+    erro.status = 404;
+    next(erro);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.mensagem
+        }
+    })
+})
+
+// app.use((error, req, res, next) => {
+//     res.status(error.status || 500);
+//     return res.send({
+//         error: {
+//             mensagem: error.message
+//         }
+//     })
+// })
 
 
 module.exports = app;
--- routes/produtos.js
+++ routes/produtos.js
@@ -2,14 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 
-// Retorna todos os produtos da loja
 router.get('/', (req, res, next) => {
     res.status(200).send({
         mensagem: 'Retorna os produtos da loja'
     });
 });
 
-// Retorna produto específico da loja
 router.get('/:id_produto', (req, res, next) => {
 
     const id = req.params.id_produto;
@@ -20,26 +18,22 @@ router.get('/:id_produto', (req, res, next) => {
     })
 })
 
-// Insere um produto
 router.post('/', (req, res, next) => {
     res.status(201).send({
         mensagem: 'Você adicionou um produto'
     });
 });
 
-// Altera um produto da loja
 router.patch('/:id_produto', (req, res, next) => {
     res.status(201).send({
         mensagem: 'Você alterou um produto'
     });
 });
 
-// Apaga um produto da loja
 router.delete('/:id_produto', (req, res, next) => {
     res.status(201).send({
         mensagem: 'Produto excluído com sucesso'
     });
 });
-
 
 module.exports = router;
